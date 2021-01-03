c7ec793ef77c3fb10dce598c91594589119458a6
NoorZak <n.zakarneh1@student.aaup.edu>
2021-01-03 14:03:19 +0200

    second

---
+++ src/com/codingdojo/web/Home.java
@@ -0,0 +1,52 @@
+package com.codingdojo.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Home
+ */
+@WebServlet("/Home")
+public class Home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Home() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String favLang = request.getParameter("favouriteLanguage");
+		String homeTown= request.getParameter("homeTown");
+		
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.write("<h1>Welcome," + firstName +" " + lastName +"</h1>"+"<h2>Your Favourite Language is:" +favLang + "</h2>"+"<h2>Your Hometown is:" + homeTown + "</h2>");
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
